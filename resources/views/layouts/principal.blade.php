7c098cd2408468151ee9ad3b27c447b01246f241
Alberto Gabriel <albertobiel00@hotmail.comgit config --global user.email albertobiel00@hotmail.com>
2024-12-19 19:16:34 -0300

    Mudança do arquivo jquery

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -34,7 +34,7 @@
   </nav>
 
   @yield('conteudo')
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="{{ asset('js/jquery.js') }}"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
